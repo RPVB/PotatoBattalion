494abcd739cb5c637412684a592df63e3066b8bf
RPVB <rpvbalen@gmail.com>
2014-06-24 13:27:57 +0200

    get tekst pakt nu teskt van elementmetid intro

--- spritz.js
+++ spritz.js
@@ -239,26 +239,10 @@ function pivot(word){
 // Shameless pinched from StackOverflow.
 function getSelectionText() {
     var text = "";
-    if (typeof window.getSelection != "undefined") {
-        var sel = window.getSelection();
-        if (sel.rangeCount) {
-            var container = document.createElement("div");
-            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
-                container.appendChild(sel.getRangeAt(i).cloneContents());
-            }
-            text = container.innerText || container.textContent;
-        }
-    } else if (typeof document.selection != "undefined") {
-        if (document.selection.type == "Text") {
-            text = document.selection.createRange().text;
-        }
-    }
-    if(text === ""){
-        return false;
-    }
-    else{
-        return text;
-    }
+    
+    text = document.getElementById("intro").innerHTML;
+       return text;
+    
 }
 
 // Uses the Readability API to get the juicy content of the current page.
